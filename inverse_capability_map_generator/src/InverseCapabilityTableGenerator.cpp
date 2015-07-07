2e04ad6fafc331a11d50074f9dcaf7a578338259
Lanners <lannersluc@gmail.com>
2015-07-07 14:18:19 +0200

    using collision checking

--- inverse_capability_map_generator/src/InverseCapabilityTableGenerator.cpp
+++ inverse_capability_map_generator/src/InverseCapabilityTableGenerator.cpp
@@ -4,10 +4,14 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Polygon.h>
 #include <tf/tf.h>
+#include <tf_conversions/tf_eigen.h>
 #include <ros/ros.h>
 #include <tclap/CmdLine.h>
 #include <string>
 #include <math.h>
+// MoveIt!
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
 
 using namespace inverse_capability_map_utils;
 
@@ -102,140 +106,253 @@ Input verifyInput(int argc, const char * const * argv)
 	return input;
 }
 
-bool robotPositionInTableContour(const tf::Point& robot, const double& width, const double& length)
+void setArmsToSide(robot_state::RobotState& robot_state)
 {
-	return -width/2 <= robot.getX() && robot.getX() <= width/2 &&
-			-length/2 <= robot.getY() && robot.getY() <= length/2;
+//	<!-- Self defined arm positions -->
+//    <group_state name="right_arm_to_side" group="right_arm">
+//		<joint name="r_shoulder_pan_joint" value="-2.110" />
+//		<joint name="r_shoulder_lift_joint" value="1.230" />
+//		<joint name="r_upper_arm_roll_joint" value="-2.06" />
+//        <joint name="r_elbow_flex_joint" value="-1.69" />  # r_upper_arm_joint
+//        <joint name="r_forearm_roll_joint" value="0.3" /> # r_elbow_flex_joint
+//        <joint name="r_wrist_flex_joint" value="-1.32" />	# r_forearm_roll_joint
+//        <joint name="r_wrist_roll_joint" value="1.57" /> # r_forearm_joint
+//    </group_state>
+//    <group_state name="left_arm_to_side" group="left_arm">
+//		<joint name="l_shoulder_pan_joint" value="2.110" />
+//        <joint name="l_shoulder_lift_joint" value="1.230" />
+//        <joint name="l_upper_arm_roll_joint" value="2.06" />
+//        <joint name="l_elbow_flex_joint" value="-1.69" />
+//        <joint name="l_forearm_roll_joint" value="-0.3" />
+//        <joint name="l_wrist_flex_joint" value="-1.32" />
+//        <joint name="l_wrist_roll_joint" value="1.57" />
+//    </group_state>
+
+	std::string arm = "right_arm";
+	const moveit::core::JointModelGroup* right_arm = robot_state.getJointModelGroup(arm);
+	std::map< std::string, double > values;
+	// right_arm_to_side is a group defined in pr2.srdf (tidyup_pr2_moveit_config)
+	if (!right_arm->getVariableDefaultPositions("right_arm_to_side", values))
+		ROS_WARN("%s: Could not set positions for %s to side!", arm.c_str());
+	std::map< std::string, double >::iterator it;
+	for (it = values.begin(); it != values.end(); it++)
+	{
+		// ROS_INFO_STREAM(it->first << " " << it->second);
+		robot_state.setJointPositions(it->first, &it->second);
+	}
+
+	arm = "left_arm";
+	const moveit::core::JointModelGroup* left_arm = robot_state.getJointModelGroup(arm);
+	if (!left_arm->getVariableDefaultPositions("left_arm_to_side", values))
+		ROS_WARN("%s: Could not set positions for %s to side!", arm.c_str());
+
+	for (it = values.begin(); it != values.end(); it++)
+	{
+		// ROS_INFO_STREAM(it->first << " " << it->second);
+		robot_state.setJointPositions(it->first, &it->second);
+	}
 }
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "inverse_capability_object_generator");
+	ros::init(argc, argv, "inverse_capability_object_generator");
 
-    Input input = verifyInput(argc, argv);
-    ros::NodeHandle nhPriv("~");
+	Input input = verifyInput(argc, argv);
+	ros::NodeHandle nhPriv("~");
 
-    // aliases
-    const double& resolution       = input.resolution;
-    polygon::bbox bbox = polygon::computeCenteredBoundingBox(*poly);
-    double widthBbox, lengthBbox;
-    widthBbox = bbox.xmax - bbox.xmin;
-    lengthBbox = bbox.ymax - bbox.ymin;
-    ROS_INFO("Bounding box width : %lf", widthBbox);
-    ROS_INFO("Bounding box length: %lf", lengthBbox);
-    polygon::center center = polygon::computeBoundingBoxCenter(bbox);
-    ROS_INFO("Bounding box center: (%lf, %lf)", center.x, center.y);
+	// parameters and aliases
+	const double& resolution       = input.resolution;
+	bool collision_checking;
+	nhPriv.param("collision_checking", collision_checking, true);
 
-    InverseCapabilityOcTree table_tree(resolution);
-    table_tree.setGroupName(object_tree->getGroupName());
-    table_tree.setBaseName(object_tree->getBaseName());
-    table_tree.setTipName(object_tree->getTipName());
-    table_tree.setThetaResolution(object_tree->getThetaResolution());
-    ROS_INFO("Group name is: %s", table_tree.getGroupName().c_str());
-    ROS_INFO("Base frame is: %s", table_tree.getBaseName().c_str());
-    ROS_INFO("Tip frame is: %s", table_tree.getTipName().c_str());
-    ROS_INFO("Resolution is: %g", table_tree.getResolution());
-    ROS_INFO("Theta resolution is: %d\n", table_tree.getThetaResolution());
+	polygon::bbox bbox = polygon::computeBoundingBox(*poly);
+	double widthBbox, lengthBbox;
+	widthBbox = bbox.xmax - bbox.xmin;
+	lengthBbox = bbox.ymax - bbox.ymin;
+	ROS_INFO("Bounding box width : %lf", widthBbox);
+	ROS_INFO("Bounding box length: %lf", lengthBbox);
+	polygon::center center = polygon::computeBoundingBoxCenter(bbox);
+	ROS_INFO("Bounding box center: (%lf, %lf)", center.x, center.y);
 
-    unsigned int width_cells, length_cells, grid_cells;
-    width_cells  = round(widthBbox / resolution);
-    length_cells = round(lengthBbox / resolution);
-    grid_cells   = width_cells * length_cells;
-    ROS_INFO("Number of width  cells: %d", width_cells);
-    ROS_INFO("Number of length cells: %d", length_cells);
-    ROS_INFO("Number of grid   cells: %d", grid_cells);
+	InverseCapabilityOcTree table_tree(resolution);
+	table_tree.setGroupName(object_tree->getGroupName());
+	table_tree.setBaseName(object_tree->getBaseName());
+	table_tree.setTipName(object_tree->getTipName());
+	table_tree.setThetaResolution(object_tree->getThetaResolution());
+	ROS_INFO("Group name is: %s", table_tree.getGroupName().c_str());
+	ROS_INFO("Base frame is: %s", table_tree.getBaseName().c_str());
+	ROS_INFO("Tip frame is: %s", table_tree.getTipName().c_str());
+	ROS_INFO("Resolution is: %g", table_tree.getResolution());
+	ROS_INFO("Theta resolution is: %d\n", table_tree.getThetaResolution());
 
-    double numCapsToCompute = width_cells * length_cells * object_tree->size();
-    double numCapsComputed = 0.0;
+	unsigned int width_cells, length_cells, grid_cells;
+	width_cells  = round(widthBbox / resolution);
+	length_cells = round(lengthBbox / resolution);
+	grid_cells   = width_cells * length_cells;
+	ROS_INFO("Number of width  cells: %d", width_cells);
+	ROS_INFO("Number of length cells: %d", length_cells);
+	ROS_INFO("Number of grid   cells: %d", grid_cells);
 
-    ROS_INFO("Number of inverse capabilities to compute: %d", (unsigned int) numCapsToCompute);
+	double numCapsToCompute = width_cells * length_cells * object_tree->size();
+	double numCapsComputed = 0.0;
 
-    // start pose at left lower corner of table, but in center of grid cell
-    geometry_msgs::Pose start_pose;
-    start_pose.position.x = center.x - widthBbox / 2 + resolution / 2;
-    start_pose.position.y = center.y - lengthBbox / 2 + resolution / 2;
-    start_pose.position.z = 0;
-    tf::Quaternion q;
-    q.setRPY(0, 0, 0);
-    tf::quaternionTFToMsg(q, start_pose.orientation);
+	ROS_INFO("Number of inverse capabilities to compute: %d", (unsigned int) numCapsToCompute);
 
-//    //  add a small value to end due to floating point precision
-//    endX += resolution/100.0;
-//    endY += resolution/100.0;
-//    endZ += resolution/100.0;
+	// progress in percent
+	double progress = 0.0;
+	double progressLimiter = 0.0;
 
-    // progress in percent
-    double progress = 0.0;
-    double progressLimiter = 0.0;
+	// Set up planning scene
+	robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+	robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+	planning_scene::PlanningScene planning_scene(kinematic_model);
 
-//    ROS_WARN_STREAM(*poly);
-//    double x = 0;
-//    double y = 0;
-//    int c = polygon::pointInPolygon(*poly, x, y);
-//
-//    if (c % 2 == 0)
-//    	ROS_INFO("point outside polygon, %d", c);
-//    else
-//    	ROS_INFO("point inside polygon, %d", c);
+	collision_detection::CollisionRequest collision_request;
+	collision_detection::CollisionResult collision_result;
 
-	geometry_msgs::Point object_in_table_frame, robot_in_object_frame;
+	robot_state::RobotState& robot_state = planning_scene.getCurrentStateNonConst();
+	// set robot state arms at side
+	setArmsToSide(robot_state);
+
+//    std::vector<std::string> names = robot_state.getVariableNames();
+//    for (int i = 0; i < names.size(); i++)
+//    {
+//    	double position = robot_state.getVariablePosition(names[i]);
+//    	ROS_INFO_STREAM(names[i] << " " << position);
+//    }
+
+	// Add polygon to planning scene
+	ROS_INFO("Planning frame: %s", planning_scene.getPlanningFrame().c_str());
+	moveit_msgs::CollisionObject co;
+	co.id = "surface";
+	co.header.frame_id = planning_scene.getPlanningFrame();
+	co.header.stamp = ros::Time::now();
+	shape_msgs::Mesh mesh = polygon::createMeshFromPolygon(*poly, 0.0, 0.03);
+	co.meshes.push_back(mesh);
+	// pose position (0, 0, 0), orientation (0, 0, 0, 1)
+	geometry_msgs::Pose pose = geometry_msgs::Pose();
+	Eigen::Affine3d e = robot_state.getGlobalLinkTransform(object_tree->getBaseName());
+	tf::Transform t;
+	tf::transformEigenToTF(e, t);
+	ROS_INFO("Torso height: %lf", t.getOrigin().z());
+	pose.position.z = t.getOrigin().z();
+	co.mesh_poses.push_back(pose);
+	co.operation = co.ADD;
+	planning_scene.processCollisionObjectMsg(co);
+
+	moveit_msgs::ObjectColor oc;
+	oc.id = co.id;
+	oc.color.r = 0.67;
+	oc.color.g = 0.33;
+	oc.color.b = 0.0;
+	oc.color.a = 1.0;
+	planning_scene.setObjectColor(oc.id, oc.color);
+
+	// start pose at left lower corner of bounding box, but in center of grid cell
+	geometry_msgs::PoseStamped start_pose;
+	start_pose.header.frame_id = planning_scene.getPlanningFrame();
+	start_pose.pose.position.x = center.x - widthBbox / 2 + resolution / 2;
+	start_pose.pose.position.y = center.y - lengthBbox / 2 + resolution / 2;
+	start_pose.pose.position.z = t.getOrigin().z();
+	tf::Quaternion q;
+	q.setRPY(0, 0, 0);
+	tf::quaternionTFToMsg(q, start_pose.pose.orientation);
+
+	geometry_msgs::PoseStamped object_in_map_frame, robot_torso_in_object_frame;
 	for (unsigned int l = 0; l != length_cells; l++)
+//	for (unsigned int l = 0; l != 1; l++)
     {
 		for (unsigned int w = 0; w != width_cells; w++)
+//		for (unsigned int w = 0; w != 1; w++)
     	{
     		// update object_pose
-			object_in_table_frame = start_pose.position;
-			object_in_table_frame.x = start_pose.position.x + w * resolution;
-			object_in_table_frame.y = start_pose.position.y + l * resolution;
+			object_in_map_frame = start_pose;
+			object_in_map_frame.pose.position.x = start_pose.pose.position.x + w * resolution;
+			object_in_map_frame.pose.position.y = start_pose.pose.position.y + l * resolution;
 
-    		// loop through all inverse capabilities
-    		for (InverseCapabilityOcTree::leaf_iterator it = object_tree->begin_leafs(); it != object_tree->end_leafs(); ++it)
-    		{
-    			// Printing progress bar
-                numCapsComputed += 1.0;
-                progress = 100.0 * numCapsComputed / numCapsToCompute;
-                if (progress > progressLimiter)
-                {
-                    progressLimiter = progress + 0.1;
-                    printf("progress: %3.2f%%\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", progress);
-                    fflush(stdout);
-                }
+			// loop through all inverse capabilities
+			for (InverseCapabilityOcTree::leaf_iterator it = object_tree->begin_leafs(); it != object_tree->end_leafs(); ++it)
+			{
+				// Printing progress bar
+				numCapsComputed += 1.0;
+				progress = 100.0 * numCapsComputed / numCapsToCompute;
+				if (progress > progressLimiter)
+				{
+					progressLimiter = progress + 0.1;
+					printf("progress: %3.2f%%\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", progress);
+					fflush(stdout);
+				}
 
-    			robot_in_object_frame.x = it.getX();
-    			robot_in_object_frame.y = it.getY();
-    			robot_in_object_frame.z = it.getZ();
+				// set header and orientation
+				robot_torso_in_object_frame = start_pose;
+				robot_torso_in_object_frame.pose.position.x = it.getX();
+				robot_torso_in_object_frame.pose.position.y = it.getY();
+				robot_torso_in_object_frame.pose.position.z = it.getZ();
 
-    			// translate robot pose into table frame, rotation of robot stays the same
-    			tf::Point robot_in_obj_frame;
-                tf::Point obj_in_table_frame;
-                tf::Point robot_in_table_frame;
+				// translate robot pose into map frame
+				ROS_ASSERT(robot_torso_in_object_frame.header.frame_id == object_in_map_frame.header.frame_id);
+				tf::Pose robot_torso_in_obj_frame;
+				tf::poseMsgToTF(robot_torso_in_object_frame.pose, robot_torso_in_obj_frame);
+				tf::Pose obj_in_map_frame;
+				tf::poseMsgToTF(object_in_map_frame.pose, obj_in_map_frame);
+				tf::Pose robot_torso_in_map_frame;
+				robot_torso_in_map_frame = robot_torso_in_obj_frame * obj_in_map_frame;
 
-                tf::pointMsgToTF(robot_in_object_frame, robot_in_obj_frame);
-                tf::pointMsgToTF(object_in_table_frame, obj_in_table_frame);
-                robot_in_table_frame = robot_in_obj_frame + obj_in_table_frame;
+				// check if robot position is inside polygon, meaning invalid position
+				// if return value of pointInPolygon is odd then position is in polygon
+				if (polygon::pointInPolygon(*poly, robot_torso_in_map_frame.getOrigin().x(), robot_torso_in_map_frame.getOrigin().y()) % 2 != 0)
+				{
+					// ROS_INFO("Position: (%lf, %lf) is in Polygon", robot_in_table_frame.getX(), robot_in_table_frame.getY());
+					continue;
+				}
 
-                // check if robot position is inside polygon, meaning invalid position
-                // if return value of pointInPolygon is odd then position is in polygon
-                if (polygon::pointInPolygon(*poly, robot_in_table_frame.getX(), robot_in_table_frame.getY()) % 2 != 0)
-                {
-//                	ROS_INFO("Position: (%lf, %lf) is in Polygon", robot_in_table_frame.getX(), robot_in_table_frame.getY());
-                	continue;
-                }
+				InverseCapability inv_obj = it->getValue();
 
-                InverseCapability inv_obj = it->getValue();
+				if (collision_checking)
+				{
+					std::map<double, double> thetas = inv_obj.getThetasPercent();
+					std::map<double, double>::iterator mit;
+					for (mit = thetas.begin(); mit != thetas.end(); mit++)
+					{
+						// full collision check, check if robot is in collision with polygon using base_link as reference frame
+						robot_state.setVariablePosition("world_joint/x", robot_torso_in_map_frame.getOrigin().x());
+						robot_state.setVariablePosition("world_joint/y", robot_torso_in_map_frame.getOrigin().y());
+						robot_state.setVariablePosition("world_joint/theta", mit->first);
+						planning_scene.setCurrentState(robot_state);
+						collision_result.clear();
+						planning_scene.checkCollision(collision_request, collision_result);
+						// if collision is found, remove (theta, percent) from thetas
+						if (collision_result.collision)
+							thetas.erase(mit);
+					}
+					// update inv_obj, in case some (theta, percent) have been removed
+					inv_obj.setThetasPercent(thetas);
+				}
 
-                // look in current tree, if inverse capability already exists, if not an empty InverseCapability is return
-                InverseCapability inv_table = table_tree.getNodeInverseCapability(robot_in_table_frame.getX(), robot_in_table_frame.getY(), robot_in_table_frame.getZ());
+				// substract torso height again, so that values are centered around torso_lift_link
+				tf::Pose robot_torso = robot_torso_in_map_frame;
+				tf::Vector3 v = robot_torso.getOrigin();
+				v.setZ(v.getZ() - t.getOrigin().z());
+				robot_torso.setOrigin(v);
 
-                InverseCapability new_inv_cap = inv_obj + inv_table;
+				// look in current tree, if inverse capability already exists, if not an empty InverseCapability is return
+				InverseCapability inv_table = table_tree.getNodeInverseCapability(robot_torso.getOrigin().x(),
+						robot_torso.getOrigin().y(),
+						robot_torso.getOrigin().z());
 
-                // add new computed InverseCapability to table tree
-                table_tree.setNodeInverseCapability(robot_in_table_frame.getX(), robot_in_table_frame.getY(), robot_in_table_frame.getZ(), new_inv_cap);
-    		}
-    	}
-    }
+				InverseCapability new_inv_cap = inv_obj + inv_table;
+
+				// add new computed InverseCapability to table tree
+				table_tree.setNodeInverseCapability(robot_torso.getOrigin().x(),
+						robot_torso.getOrigin().y(),
+						robot_torso.getOrigin().z(),
+						new_inv_cap);
+			}
+		}
+	}
 
 	double max_percent;
+	size_t num_inv_cap = 0;
 	// normalize nodes
 	for (InverseCapabilityOcTree::leaf_iterator it = table_tree.begin_leafs(); it != table_tree.end_leafs(); ++it)
 	{
@@ -245,10 +362,14 @@ int main(int argc, char** argv)
 		const std::pair<double, double> pair = it->getInverseCapability().getMaxThetaPercent();
 		if (max_percent < pair.second)
 			max_percent = pair.second;
+		// Count the number of inverse capabilities
+		num_inv_cap += it->getInverseCapability().getThetasPercent().size();
 	}
 	table_tree.setMaximumPercent(max_percent);
 
     printf("done              \n");
+    ROS_INFO("Maximum percent of inverse capability: %lf", max_percent);
+    ROS_INFO("Number of total inverse capabilities: %lu", num_inv_cap);
 
     if (!table_tree.writeFile(input.path_name))
     {
